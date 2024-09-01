fed7769e92f2221750d49ceacac340ab23343f8c
Gloireski <belemgloirebekoutou8@gmail.com>
2024-09-01 18:49:22 +0100

    reset eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,11 +1,12 @@
 module.exports = {
   env: {
-    browser: true,
-    commonjs: true,
+    browser: false,
     es6: true,
+    jest: true,
   },
   extends: [
     'airbnb-base',
+    'plugin:jest/all',
   ],
   globals: {
     Atomics: 'readonly',
@@ -13,8 +14,25 @@ module.exports = {
   },
   parserOptions: {
     ecmaVersion: 2018,
+    sourceType: 'module',
   },
+  plugins: ['jest'],
   rules: {
     'linebreak-style': 0,
+    'max-classes-per-file': 'off',
+    'no-underscore-dangle': 'off',
+    'no-console': 'off',
+    'no-shadow': 'off',
+    'no-restricted-syntax': [
+      'error',
+      'LabeledStatement',
+      'WithStatement',
+    ],
   },
+  overrides: [
+    {
+      files: ['*.js'],
+      excludedFiles: 'babel.config.js',
+    },
+  ],
 };
