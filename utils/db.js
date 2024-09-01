20b018df69fbf0746c05cc5ab11d00df8bd8252d
Gloireski <belemgloirebekoutou8@gmail.com>
2024-09-02 00:01:44 +0100

    task 1

---
+++ utils/db.js
@@ -0,0 +1,45 @@
+import { MongoClient } from 'mongodb';
+// const { MongoClient } = require('mongodb');
+
+/**
+ * Represents a mongoDb client
+ */
+class DBClient {
+  /**
+   * creates new mongodb instance
+   */
+  constructor() {
+    const host = process.env.DB_HOST || 'localhost';
+    const port = process.env.DB_PORT || 27017;
+    const database = process.env.DB_DATABASE || 'files_manager';
+    const url = `mongodb://${host}:${port}:${database}`;
+    this.client = new MongoClient(url);
+    this.client.connect();
+  }
+
+  /**
+   * checks if the client conn to mongo is active
+   * @returns {boolean}
+   */
+  isAlive() {
+    return this.client.isConnected();
+  }
+
+  /**
+   *  returns the number of docs in the collection users
+   *  @returns {integer}
+   */
+  async nbUsers() {
+    return this.client.db().collection('users').countDocuments();
+  }
+
+  /**
+   * returns the numbers of docs in the collections files
+   * @returns {integer}
+   */
+  async nbFiles() {
+    return this.client.db().collection('files').countDocuments();
+  }
+}
+const dbClient = new DBClient();
+module.exports = dbClient;
