9af022168acda8580571b5676e349c48c55f5447
Gloireski <belemgloirebekoutou8@gmail.com>
2024-09-03 00:27:15 +0100

    task 3 fixing6

--- app.js
+++ app.js
@@ -1,10 +1,40 @@
-const name = 'James';
+/* eslint-disable import/no-import-module-exports */
+import sha1 from 'sha1';
+import dbClient from '../utils/db';
 
-const person = { first: name };
+/**
+ * Contains the users route handlers.
+ */
+class UsersController {
+  /**
+     * should create a new user in DB
+     * @param {*} request
+     * @param {*} response
+     */
+  static async postNew(request, response) {
+    const email = request.body ? request.body.email : null;
+    const password = request.body ? request.body.password : null;
 
-console.log(person);
+    if (!email) {
+      response.status(400).json({ error: 'Missing email' });
+      return;
+    }
+    if (!password) {
+      response.status(400).json({ error: 'Missing password' });
+      return;
+    }
+    const user = await (await dbClient.usersController())
+      .findOne({ email });
+    if (user) {
+      response.status(400).json({ error: 'Already exist' });
+      return;
+    }
+    const insertInfo = await (await dbClient.usersController())
+      .insertOne({ email, password: sha1(password) });
+    const userId = insertInfo.insertedId.toString();
+    response.status(200).json({ email, id: userId });
+  }
+}
 
-const sayHelloLinting = (fName) => {
-  console.log(`Hello linting, ${fName}`);
-};
-sayHelloLinting();
+export default UsersController;
+module.exports = UsersController;
--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -1,40 +1,29 @@
-/* eslint-disable import/no-import-module-exports */
+/* eslint-disable import/no-named-as-default */
 import sha1 from 'sha1';
 import dbClient from '../utils/db';
 
-/**
- * Contains the users route handlers.
- */
-class UsersController {
-  /**
-     * should create a new user in DB
-     * @param {*} request
-     * @param {*} response
-     */
-  static async postNew(request, response) {
-    const email = request.body ? request.body.email : null;
-    const password = request.body ? request.body.password : null;
+export default class UsersController {
+  static async postNew(req, res) {
+    const email = req.body ? req.body.email : null;
+    const password = req.body ? req.body.password : null;
 
     if (!email) {
-      response.status(400).json({ error: 'Missing email' });
+      res.status(400).json({ error: 'Missing email' });
       return;
     }
     if (!password) {
-      response.status(400).json({ error: 'Missing password' });
+      res.status(400).json({ error: 'Missing password' });
       return;
     }
-    const user = await (await dbClient.usersController())
-      .findOne({ email });
+    const user = await (await dbClient.usersCollection()).findOne({ email });
+
     if (user) {
-      response.status(400).json({ error: 'Already exist' });
+      res.status(400).json({ error: 'Already exist' });
       return;
     }
-    const insertInfo = await (await dbClient.usersController())
+    const insertionInfo = await (await dbClient.usersCollection())
       .insertOne({ email, password: sha1(password) });
-    const userId = insertInfo.insertedId.toString();
-    response.status(200).json({ email, id: userId });
+    const userId = insertionInfo.insertedId.toString();
+    res.status(201).json({ email, id: userId });
   }
 }
-
-export default UsersController;
-module.exports = UsersController;
