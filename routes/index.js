dc8835b8429a1470b97f7140d93bde0210088ad5
Gloireski <belemgloirebekoutou8@gmail.com>
2024-09-03 10:48:23 +0100

    task 3 reset

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,4 @@
-/* eslint-disable import/no-import-module-exports */
 import AppController from '../controllers/AppController';
-// import UsersController from '../controllers/UsersController';
 
 /**
  * Binds the routes to the appropriate handler in the
@@ -10,7 +8,6 @@ import AppController from '../controllers/AppController';
 const mapRoutes = (app) => {
   app.get('/status', AppController.getStatus);
   app.get('/stats', AppController.getStats);
-  // app.post('/users', UsersController.postNew);
 };
 
 export default mapRoutes;
--- server.js
+++ server.js
@@ -1,4 +1,3 @@
-// eslint-disable-next-line import/no-import-module-exports
 import mapRoutes from './routes';
 
 const express = require('express');
@@ -7,7 +6,6 @@ const express = require('express');
  * create an express server
  */
 const app = express();
-app.use(express.json({ limit: '200mb' }));
 const port = process.env.PORT || 5000;
 
 mapRoutes(app);
